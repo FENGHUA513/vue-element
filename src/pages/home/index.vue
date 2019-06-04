1732312d3268370dfc134da54df0cbeff271dbfd
jing <jing.com>
2019-06-04 15:30:22 +0800

    默认显示用户管理模块

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,7 +10,7 @@
             </el-header>
             <el-container style="height: 500px; border: 1px solid #eee;">
                 <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-                    <el-menu :default-openeds="['1', '3']">
+                    <el-menu default-active="1">
                         <el-menu-item index="1">
                             <i class="el-icon-menu"></i>
                             <router-link class="item" :to="{path: '/user'}" exact replace>用户管理</router-link>
